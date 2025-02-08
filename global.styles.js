09c0f636d22bb6aebeb66030e33e24b2ef33cfac
Olena457 <olenademianenko42@gmail.com>
2025-02-08 21:06:41 +0200

    change structure

--- global.styles.js
+++ global.styles.js
@@ -2,14 +2,6 @@ const variablesStyles = '@import "./src/scss/common/_vars.scss"';
 const breakpointsStyles = '@import "./src/scss/common/_breakpoints.scss"';
 const mixinsStyles = '@import "./src/scss/common/_mixins.scss"';
 const resetStyles = '@import "./src/scss/common/_reset.scss"';
-
-const styles = [
-  variablesStyles,
-  breakpointsStyles,
-  //   colorsStyles,
-  mixinsStyles,
-  //   baseStyles,
-  resetStyles,
-];
+const styles = [variablesStyles, breakpointsStyles, mixinsStyles, resetStyles];
 
 export const globalStylesOptions = styles.reduce((acc, i) => acc + i + ';', '');
