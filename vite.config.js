ff56fccfdcf59be43e5ecbb424ccc08cf95ea943
Olena457 <olenademianenko42@gmail.com>
2025-02-08 23:01:36 +0200

    added section

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import { globalStylesOptions } from './global.styles';
+import { globalStylesOptions } from './global.styles.js';
 
 export default defineConfig(({ command }) => {
   return {
